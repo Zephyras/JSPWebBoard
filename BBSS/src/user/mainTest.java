6dbde922893e649c3ee72fae15b9de42926b8666
hkw0101 <ghkdroqkf@gmail.com>
2018-12-06 17:56:07 +0900

    push

--- BBSS/src/user/mainTest.java
+++ BBSS/src/user/mainTest.java
@@ -1,8 +1,26 @@
 package user;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class mainTest {
 //여기서 이 화면 우클릭 하고 run as  java어플리케이션으로 실행하세요
+	private static String userID;
+	private static String userPassword;
+	private String userName;
+	private String userGender;
+	private String userEmail;
+	
 	public static void main(String[] args) {
+		Connection conn = null;
+		PreparedStatement pstmt;
+		ResultSet rs;
+		
+		
+		
+		
 		try {
 			String dbURL = "jdbc:mysql://127.0.0.1:3306/BBSS";
 			String dbID = "root";
@@ -33,5 +51,36 @@ public class mainTest {
 		System.out.println(-2);//데이터베이스 오류
 
 	}
-
+	
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public String getUserPassword() {
+		return userPassword;
+	}
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserGender() {
+		return userGender;
+	}
+	public void setUserGender(String userGender) {
+		this.userGender = userGender;
+	}
+	public String getUserEmail() {
+		return userEmail;
+	}
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}	
+	
 }
